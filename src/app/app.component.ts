69767fb5be5d94521a379472b6bf0846021c6c30
fabio circuncisao <fabio.circuncisao@aditek.com.br>
2024-04-24 07:29:45 -0300

    modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { TQuery } from 'src/Types/search.types';
+import { ILocation } from 'src/_interfaces/ILocation';
+import { WeatherApiService } from 'src/services/weather.api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'weather';
+  locals: ILocation = {} as ILocation;
+
+  @Input() text:string = "Search";
+  Aqi: "yes" | "no" = "yes";
+
+  @Input() query:string = "";
+  @Input() sub:string = "";
+  details:string = ""
+
+
+  formGroup!: FormGroup;
+  constructor (private http: WeatherApiService) {}
+  ngOnInit() {
+    this.formGroup = new FormGroup({
+      nameCity: new FormControl('',[Validators.required])
+    })
+  }
+
+  get InputText() {
+    return this.formGroup.get('nameCity')!
+  }
+
+  fetchWeatherData() {
+    if(this.formGroup.invalid) return;
+
+    try {
+      const payload: TQuery = {
+        nameCity: String(this.formGroup.controls['nameCity'].value),
+      }
+
+      this.http.FilterWeather(payload, this.Aqi).subscribe({ next:(data) => this.locals = data })
+    } catch (error) {
+      console.log(error)
+    } finally {
+      this.formGroup.reset()
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TextComponent } from './components/text/text.component';
+import { FilterPipe } from './filter.pipe';
+import { ContainerViewWeatherComponent } from './components/container-view-weather/container-view-weather.component';
+import { HttpClientModule } from '@angular/common/http';
+import { DetailsComponent } from './components/details/details.component';
+import { LoadingPageComponent } from './components/loading-page/loading-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    TextComponent,
+    FilterPipe,
+    ContainerViewWeatherComponent,
+    DetailsComponent,
+    LoadingPageComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
